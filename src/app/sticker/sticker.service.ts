7ca124a010fd316bae401622636dccdcf0b605dc
eller <martin@ellermeier.net>
2017-01-07 06:23:08 +0100

    Renaming sticker-mock.ts to stickers-mock-data.ts

--- src/app/sticker/sticker.service.ts
+++ src/app/sticker/sticker.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { STICKERS } from './sticker-mock';
+import { STICKERS } from './stickers-mock-data';
 import { Sticker } from "./sticker";
 
 @Injectable()
